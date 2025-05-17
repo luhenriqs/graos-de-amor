e48320c739815a3dacc8aab3dc42f5ac4f4fc015
Lucas Henrique Damasceno <lucashdamasceno@hotmail.com.br>
2025-05-16 22:27:18 -0300

    Mudei a home a adicinei imagem

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,13 +5,13 @@ import { Component } from '@angular/core';
   standalone: true,
   template: `
     <div class="paddings-container">
-      <div class="padding-section" routerLink="/cadastro" style="background-image: url('assets/img-cadastro.png')">
+      <div class="padding-section" routerLink="/cadastro" style="background-image: url('/assets/Cadastro.png')">
         <h2>Cadastro</h2>
       </div>
-      <div class="padding-section" routerLink="/cardapio" style="background-image: url('assets/img-cardapio.png')">
+      <div class="padding-section" routerLink="/cardapio" style="background-image: url('/assets/Cardapio.jpeg')">
         <h2>Cardápio</h2>
       </div>
-      <div class="padding-section" routerLink="/sobre-nos" style="background-image: url('assets/img-sobrenos.jpeg')">
+      <div class="padding-section" routerLink="/sobre-nos" style="background-image: url('/assets/img-sobrenos.jpeg')">
         <h2>Sobre Nós</h2>
       </div>
     </div>
